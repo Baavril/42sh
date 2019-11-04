2196ad29965edd2ac82db68f6d7d4cb5e4575aab
G89Cl215 <thomas.gwed@gmail.com>
2019-11-04 19:34:30 +0100

    Fix(alias & unalias):
    Fixed display.
    Fixed options using getopt.

--- src/job/builtins/builtin_alias.c
+++ src/job/builtins/builtin_alias.c
@@ -6,11 +6,12 @@
 /*   By: tgouedar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/02 13:45:27 by tgouedar          #+#    #+#             */
-/*   Updated: 2019/11/04 16:39:22 by tgouedar         ###   ########.fr       */
+/*   Updated: 2019/11/04 19:33:00 by tgouedar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <getopt.h>
 #include "job.h"
 #include "libft.h"
 #include "error.h"
@@ -32,6 +33,8 @@
 */
 
 t_htable		*g_alias = NULL;
+extern int		optind;
+extern int		opterr;
 
 void				ft_treat_alias(char **first_arg)
 {
@@ -67,21 +70,37 @@ void			ft_free_aliastable(void)
 
 int			cmd_unalias(int ac, char **av)
 {
-	_Bool		ret;
+	int			ret;
 	int			i;
+	char		*opt;
 
-	if (ac == 1)
+	opt = "+a";
+	opterr = 0;
+	optind = 1;
+	while ((i = getopt(ac, av, opt)) > 0)
 	{
-		ft_dprintf(STDERR_FILENO, "unalias: usage: unalias [-a] name [name ...]\n");
+		if (i == '?' && (ret = 2))
+			break ;
+		if (i == 'a')
+		{
+			ft_empty_htable(g_alias);
+			return (0);
+		}
+	}
+	if (ac < 2 || ret == 2)
+	{
+		ft_dprintf(STDERR_FILENO, "unalias: usage: unalias [-a] name ... ]\n");
 		return (2);
 	}
-	i = 0;
+	if (!g_alias)
+		return (0);
+	i = optind;
 	ret = 0;
 	while (i < ac)
 	{
 		if (!ft_del_entry(g_alias, av[i]))
 		{
-				ft_dprintf(STDERR_FILENO, "%s: unalias: %s: not found\n", g_progname, av[i]);
+			ft_dprintf(STDERR_FILENO, "%s: unalias: %s: not found\n", g_progname, av[i]);
 			ret |= 1;
 		}
 		i++;
@@ -91,7 +110,7 @@ int			cmd_unalias(int ac, char **av)
 
 int			ft_isvalid_aliasname(char *name)
 {
-	size_t	i;
+	size_t		i;
 
 	i = 0;
 	while (name[i])
@@ -103,53 +122,63 @@ int			ft_isvalid_aliasname(char *name)
 	return ((i > 0));
 }
 
+static void		ft_print_sortalias(char *mes)
+{
+	t_list		*entries;
+	t_list		*voyager;
+	t_entry		*content;
+	int			table_type;
+
+	if (!g_alias)
+		return ;
+	table_type = g_alias->data_type;
+	entries = (t_list*)ft_lst_entries(g_alias);
+	ft_lst_mergesort(&entries, ft_get_lstcmp(table_type));
+	voyager = entries;
+	while (voyager)
+	{
+		content = voyager->content;
+		ft_printf(mes, content->key, content->value);
+		voyager = voyager->next;
+	}
+	ft_lstdel(&entries, ft_get_free(table_type));
+}
+
 int			cmd_alias(int ac, char **av)
 {
 	int			i;
-//	int			j;
-	_Bool		ret;
+	int			ret;
 	char		*str;
 	char		*mes;
+	char		*opt;
 
+	opt = "+p";
+	mes = "%s='%s'\n";
 	if (ac < 1)
 		return (1);
 	if (ac == 1)
 	{
-		if (g_alias)
-			ft_print_sortentries(g_alias);
+		ft_print_sortalias(mes);
 		return (0);
 	}
-	
-	i = 1;
-	ret = 0;
-	mes = "%s='%s'\n";
-//	/**gestion de l'option: peut etre edulcoree ? */
-//	while (av[i] && av[i][0] == '-' && (ft_strcmp(av[i], "--") || (i++)))
-//	{
-//		j = 1;
-//		while (av[i][j])
-//		{
-//			if (av[i][j] == 'p')
-//			{
-//				mes = "alias %s='%s'\n";
-//				ret = 1;
-//			}
-//			else
-//			{
-//				ft_dprintf(STDERR_FILENO, "alias: usage: alias [-p] [name[=value] ... ]");
-//				return (2);
-//			}
-//			j++;
-//		}
-//		i++;
-//	}
-//	if (ret && !(ret = 0) && g_alias)
-//	ft_print_sortentries(g_alias);
-//
-//	/* Fin de gestion de l'option */
-
-	/* Gestion des arguments */
-
+	opterr = 0;
+	optind = 1;
+	while ((ret = getopt(ac, av, opt)) > 0)
+	{
+		if (ret == '?')
+		{
+			ft_dprintf(STDERR_FILENO, "alias: usage: alias [-p] [name[=value] ... ]\n");
+			return (2);
+		}
+		if (ret == 'p')
+		{
+			mes = "alias %s='%s'\n";
+			i = 1;
+		}
+	}
+	if (i && !(ret = 0))
+		ft_print_sortalias(mes);
+	i = optind;
 	while (i < ac)
 	{
 		if ((str = ft_strchr(av[i], '=')))
