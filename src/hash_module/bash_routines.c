384bdafba3b406ca73836aba0d1c9bb5cb3816d6
Ant0wan <abarthel@student.42.fr>
2019-11-06 12:33:46 +0100

    fix(hash): indentify segv in bash_routine
    Fix on the go. SegV produced by a heap-buffer-overflow

--- src/hash_module/bash_routines.c
+++ src/hash_module/bash_routines.c
@@ -6,7 +6,7 @@
 /*   By: tgouedar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 18:00:54 by tgouedar          #+#    #+#             */
-/*   Updated: 2019/10/30 15:07:06 by tgouedar         ###   ########.fr       */
+/*   Updated: 2019/11/06 12:32:54 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ t_list		*ft_bash_lstcpy(t_list *elem)
 	t_bash_hash		*tmp;
 
 	new = (t_hlist*)elem;
-	new->content->key = ft_strdup(new->content->key);
+	new->content->key = ft_strdup(new->content->key); /* Line that produce SegV */
 	tmp = ft_memalloc(new->content->value_size);
 	ft_memcpy(tmp, new->content->value, new->content->value_size);
 	tmp->bin_path = ft_strdup(tmp->bin_path);
