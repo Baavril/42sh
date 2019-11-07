f2f14f6903809fc5a1fc17f6c9f2f08c2026c0b9
G89Cl215 <thomas.gwed@gmail.com>
2019-11-07 10:51:16 +0100

    Fix(hash):
    identified final bug coming from libft ft_lstmap

--- libft/src/ds_singly_linked_list/ft_lstmap.c
+++ libft/src/ds_singly_linked_list/ft_lstmap.c
@@ -22,7 +22,7 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 		new = ft_lstnew(lst->content, lst->content_size);
 		if (!new)
 			return (NULL);
-		new = f(lst);
+		new = f(new);
 		new->next = ft_lstmap(lst->next, f);
 	}
 	return (new);
