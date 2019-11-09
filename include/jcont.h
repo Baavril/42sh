cab8b26101ee82dfadad66b90170faf9f255563d
G89Cl215 <thomas.gwed@gmail.com>
2019-11-09 18:37:18 +0100

    Feat(jobcontrol):
    First commit on dev branch for job control.

--- include/jcont.h
+++ include/jcont.h
@@ -6,15 +6,44 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/16 13:12:45 by abarthel          #+#    #+#             */
-/*   Updated: 2019/07/16 13:49:44 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/11/09 13:59:19 by tgouedar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef JCONT_H
 # define JCONT_H
 
-int	jcont(char **cmd, char **envp);
+# define COMPLETE			0x1
+# define IFCOMPLETE(state)	(state & COMPLETE)
+# define STOPPED			0x2
+# define IFSTOPPED(state)	(state & STOPPED)
 
-extern int	g_retval;
+
+
+typedef struct				s_process
+{
+	struct s_proces			*next;       /* next process in pipeline */
+	char					**argv;      /* for exec */
+	pid_t					pid;         /* process ID */
+	char					state;     /* true if process has stopped */
+	int						ret_val;      /* reported status value */
+}							t_process;
+
+typedef struct				s_job
+{
+	struct s_job			*next;              /* next active job */
+	char					*command;           /* command line, used for messages */
+	t_process				*first_process;     /* list of processes in this job */
+	pid_t					pgid;               /* process group ID */
+	char					notified;           /* true if user told about stopped job */
+	struct termios			tmodes;             /* saved terminal modes */
+	int						fd_stdin;
+	int						fd_stdout;
+	int						fd_stderr;
+}							t_job;
+
+int							jcont(char **cmd, char **envp);
+
+extern int					g_retval;
 
 #endif
--- src/jcont/jcont.c
+++ src/jcont/jcont.c
@@ -6,14 +6,17 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 17:59:39 by abarthel          #+#    #+#             */
-/*   Updated: 2019/07/21 13:23:31 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/11/09 18:35:02 by tgouedar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "job.h"
 
-static char **position_token(char **cmd)
+pid_t				g_cur_gpid = 0;
+t_list				*g_bg_jobs = NULL;
+
+static char			**position_token(char **cmd)
 {
 	int	i;
 
@@ -37,7 +40,7 @@ static char **position_token(char **cmd)
 	return (&(cmd[i]));
 }
 
-static char **jump_sep(char **cmd)
+static char			**jump_sep(char **cmd)
 {
 	int	i;
 
@@ -49,7 +52,7 @@ static char **jump_sep(char **cmd)
 	return (&cmd[i]);
 }
 
-static char	**ft_sequence(char **cmd)
+static char			**ft_sequence(char **cmd)
 {
 	char **seq;
 	int	i;
@@ -69,15 +72,70 @@ static char	**ft_sequence(char **cmd)
 	return (seq);
 }
 
-int	jcont(char **cmd, char **envp)
+static int			ft_build_job(t_job *pipeline, char **cmd, size_t *i)
 {
+	size_t		pipe_start;
+	size_t		pipe_end;
+
+	if (!ft_strcmp(cmd[*i], "&") && )
+		return (0);
+	pipe_start = (ft_ispipesepar(cmd[*i]) && (*i)) ? ++(*i) : *i;
+	pipe_end = pipe_start;
+	while (cmd[pipe_end] && !ft_ispipesepar(cmd[pipe_end]))
+		pipe_end++;
+	*i = pipe_end;
+	if (pipe_end == pipe_start)
+	{
+		//syntax error near cmd[pipe_end]
+		return (0); // cas d'un sep en debut ou de deux seps en suivant
+	}
+	pipeline->command = ft_tab_range_cpy(cmd, pipe_start, pipe_end - 1);
+	pipeline->process = NULL;
+	// Coment set le groupe pid ?
+	pipeline->notified = !ft_strcmp(cmd[pipe_end], "&") ? 1 : 0;
+	// Copy de term_mode;
+	pipeline->fd_stdin = 0;
+	pipeline->fd_stdout = 1;
+	pipeline->fd_stderr = 2;
+	pipeline->ret_val = -1;
+	return (1);
+}
+
+
+
+
+int					jcont(char **cmd, char **envp)
+{
+	t_job	cur_job;
 	char	**argv;
 	int		ret;
 
 	ret = 0;
+
+	size_t		i;
+
+	i = 0;
+	while (i < ac && ft_build_job(&cur_job, cmd, &i))
+	{
+		ft_build_process(&cur_job, cmd, &i);
+		ret = exec_pipeline(cur_job);
+		if (IFCOMPLETE(ret))
+			ft_reset_pipeline(&cur_job);
+		else if (IFSTOPPED(ret))
+			ft_lstadd(&g_bg_jobs, ft_lstnew(&cur_job, sizeof(t_job)));
+	}
+
+
+
+
+
 	cmd = jump_sep(cmd);
+
+
+
+
 	while (cmd && *cmd)
- 	{
+	{
 		argv = ft_sequence(cmd);
 		if (**argv)
 			ret = job(argv, envp);
