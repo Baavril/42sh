9c9e410e6552f49098c7fefa185f044b5d61252e
bprunevi <bprunevi@42l.fr>
2019-11-18 16:50:02 +0100

    fix(inhibiteurs)

--- src/input/quote.c
+++ src/input/quote.c
@@ -6,7 +6,7 @@
 /*   By: bprunevi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 11:16:45 by bprunevi          #+#    #+#             */
-/*   Updated: 2019/11/09 17:44:31 by baavril          ###   ########.fr       */
+/*   Updated: 2019/11/18 16:49:00 by bprunevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,16 @@
 
 static char	next_valid_entry(char current_char, char previous_valid_entry)
 {
-	//ft_printf("nva(%c,%c)", current_char, previous_valid_entry);
-	if (previous_valid_entry == '`' || previous_valid_entry == '\"' || previous_valid_entry == '\'')
-	{
-		if (current_char == previous_valid_entry)
-			return(-1);
-	}
-	else if (previous_valid_entry == '{' && current_char == '}')
+	if (previous_valid_entry == '\\'
+	|| ((  previous_valid_entry == '`' 
+		|| previous_valid_entry == '\"'
+		|| previous_valid_entry == '\'')
+		&& current_char == previous_valid_entry)
+	|| (previous_valid_entry == '{' && current_char == '}')
+	|| (previous_valid_entry == '(' && current_char == ')'))
 		return(-1);
-	else if (previous_valid_entry == '(' && current_char == ')')
-		return(-1);
-	else if (ft_strchr("{(\'\"`", current_char))
-		return (current_char);
+	else if (ft_strchr("{(\'\"`\\\\", current_char))
+			return (current_char);
 	return(0);
 }
 
@@ -36,15 +34,10 @@ char *quote_prompt(char *command)
 	char *buff;
 	int size;
 
-	size = ft_strlen(command) + 1;
-	if (!(buff = (char *)malloc(sizeof(char) * size)))
+	size = ft_strlen(command);
+	if (!(buff = (char *)malloc(sizeof(char) * (size + 1))))
 		return (NULL);
-	ft_bzero(buff, size);
-	if (command[size - 1] == '\\')
-	{
-		command[size - 1] = '\0';
-		buff[0] = '\\';
-	}
+	ft_bzero(buff, size + 1);
 	while (*command)
 	{
 		c = next_valid_entry(*(command++) , buff[0] ? buff[ft_strlen(buff) - 1] : buff[0]);
@@ -53,6 +46,9 @@ char *quote_prompt(char *command)
 		else if (c)
 			buff[ft_strlen(buff)] = c;
 	}
+	if (buff[0])
+		if (buff[ft_strlen(buff) - 1] == '\\')
+			*(command - 1) = '\0';
 	if (buff[0])
 		return (buff);
 	free(buff);
