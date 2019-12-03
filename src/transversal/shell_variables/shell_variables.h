d1d5252834c2b1657cbb8a2b76df2ff0991323a7
Baptiste <baavril@student.42.fr>
2019-12-03 16:53:13 +0100

    feat(shellvars): init shell_variables dir
    init structure to manage shell variables in shell_variables.h

--- src/transversal/shell_variables/shell_variables.h
+++ src/transversal/shell_variables/shell_variables.h
@@ -6,24 +6,20 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 09:48:01 by abarthel          #+#    #+#             */
-/*   Updated: 2019/11/08 17:53:54 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/12/03 16:46:35 by baavril          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHELL_VARIABLES_H
 # define SHELL_VARIABLES_H
 
-# include <stdint.h>
-
-# include "ft_btree.h"
-
-extern struct s_btree	*g_shellvariables;
-
-struct s_shvar
+typedef struct	s_shevar
 {
 	char		*str;
-	uint64_t	index:63;
-	_Bool		isarray:1;
-} __attribute__((aligned(16)));
+	char		*name;
+	char		*value;
+	int		expt;
+	int		set;
+}		t_shevar;
 
 #endif
