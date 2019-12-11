79fc0f86140721d1c0c4159a9e5f0de40a23168d
Baptiste <baavril@student.42.fr>
2019-12-11 16:27:02 +0100

    feat(expansions): added colon-equal expansion
    The colon-equal expansion, i.e. ${var_name:=word} substitute or a shell variable to the intern vars if var_name does not exist or is equal to NULL. Otherwise it expands the var_name.
    Leaks are not fixed.
    debug print are still active.

--- src/expansions/substitutions.c
+++ src/expansions/substitutions.c
@@ -12,6 +12,7 @@
 
 #include "expansions.h"
 #include "shell_variables.h"
+#include "builtins.h"
 #include "libft.h"
 
 int		direct_exp(char **token)
@@ -98,12 +99,76 @@ int		dash_exp(char **token)
 	return (ERROR);
 }
 
+char	*setasvar(char *token)
+{
+	int i;
+	int j;
+	int cpy;
+	char *ret;
+	char *keep;
+
+	i = 0;
+	j = 0;
+	cpy = 0;
+	ret = NULL;
+	keep = ft_strchr(token, EQUAL) + 1;
+	keep = ft_strndup(keep, ft_strlen(keep) - 1);
+	while (token[i] && !(ft_isalpha(token[i])))
+		i++;
+	j = 1;
+	cpy = i;
+	while (token[i] && (ft_isalpha(token[i])))
+	{
+		j++;
+		i++;
+	}
+	if (!(ret = (char *)ft_memalloc(sizeof(char) * (j + 2))))
+		return (0);
+	i = 0;
+	while (token[cpy] && i < j - 1)
+		ret[i++] = token[cpy++];
+	ret[i] = EQUAL;
+	ret[i + 1] = '\0';
+	ret = ft_strjoin(ret, keep);
+	ft_strdel(&keep);
+	return (ret);
+}
+
 int		equal_exp(char **token)
 {
-	(void)token;
+	char *keep;
+	char *nod;
+	struct s_svar *tmp;
 
-	ft_printf("equal success");
-	return (SUCCESS);
+	tmp = g_svar;
+	nod = setasvar(*token);
+	keep = ft_strchr(*token, EQUAL) + 1;
+	while (g_svar)
+	{
+		if (ft_strncmp(g_svar->key, &(token[0])[2], ft_strlen(g_svar->key) - 1) == 0)
+		{
+			ft_strdel(token);
+			if (g_svar->value)
+				*token = ft_strdup(g_svar->value);
+			else
+			{
+				*token = ft_strdup(keep);
+				g_svar = tmp;
+				if (checkvarlst(nod))
+					listadd_back(newnodshell(nod, 0));
+			}
+			g_svar = tmp;
+			cmd_set(0, NULL);
+			return (SUCCESS);
+		}
+		g_svar = g_svar->next;
+	}
+	ft_strdel(token);
+	*token = ft_strndup(keep, ft_strlen(keep) - 1);
+	g_svar = tmp;
+	listadd_back(newnodshell(nod, 0));
+	cmd_set(0, NULL);
+	return (ERROR);
 }
 
 int		bsharp_exp(char **token)
