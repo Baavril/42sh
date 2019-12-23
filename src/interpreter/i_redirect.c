78c582d90b8392af8c1f3c60ee281cfc69f44cce
bprunevi <bprunevi@42l.fr>
2019-12-23 17:14:47 +0100

    fix(interpreter) redirect upgrade

--- src/interpreter/i_redirect.c
+++ src/interpreter/i_redirect.c
@@ -6,7 +6,7 @@
 /*   By: bprunevi <bprunevi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/19 12:08:47 by bprunevi          #+#    #+#             */
-/*   Updated: 2019/12/22 15:30:26 by bprunevi         ###   ########.fr       */
+/*   Updated: 2019/12/23 17:14:39 by bprunevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 #include "libft.h"
 #include <unistd.h>
 #include <fcntl.h>
+#define CREATE_RIGHTS S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
+
+int open_on_fd(const char *path, int o_flags, mode_t mode, int final_fd)
+{
+	int open_fd;
+
+	if ((open_fd = open(path, o_flags, mode)) != -1)
+	{
+		dup2(open_fd, final_fd);
+		close(open_fd);
+		return(final_fd);
+	}
+	return(-1);
+}
 
 int	i_less(t_elem left, t_elem right)
 {
@@ -23,7 +37,7 @@ int	i_less(t_elem left, t_elem right)
 	if (!access(right.c, R_OK))
 	{
 		if (is_regfile(right.c))
-			return (dup2(open(right.c, O_RDONLY), fd1));
+			return (open_on_fd(right.c, O_RDONLY, 0, fd1));
 		ft_printf("%s exists and is not a regular file\n", right.c);
 	}
 	else if (!access(right.c, F_OK))
@@ -38,9 +52,10 @@ int	i_lessand(t_elem left, t_elem right)
 	int fd1;
 
 	fd1 = left.c ? *left.c - '0' : 0;
-	if (dup2(*right.c - '0', fd1) == -1)
-		ft_printf("error : %s : incorrect fd\n", right.c);
-	return (0);
+	if (dup2(*right.c - '0', fd1) != -1)
+		return (fd1);
+	ft_printf("error : %s : incorrect fd\n", right.c);
+	return (-1);
 }
 
 int	i_great(t_elem left, t_elem right)
@@ -51,13 +66,13 @@ int	i_great(t_elem left, t_elem right)
 	if (!access(right.c, W_OK))
 	{
 		if (is_regfile(right.c))
-			return (dup2(open(right.c, O_WRONLY), fd1));
+			return (open_on_fd(right.c, O_WRONLY, 0, fd1));
 		ft_printf("%s exists and is not a regular file\n", right.c);
 	}
 	else if (!access(right.c, F_OK))
 		ft_printf("%s exists but is not a writeable file\n", right.c);
 	else
-		return (dup2(open(right.c, O_WRONLY | O_CREAT), fd1));
+		return (open_on_fd(right.c, O_WRONLY | O_CREAT, CREATE_RIGHTS, fd1));
 	return (-1);
 }
 int	i_dgreat(t_elem left, t_elem right)
@@ -68,13 +83,13 @@ int	i_dgreat(t_elem left, t_elem right)
 	if (!access(right.c, W_OK))
 	{
 		if (is_regfile(right.c))
-			return (dup2(open(right.c, O_WRONLY | O_APPEND), fd1));
+			return (open_on_fd(right.c, O_WRONLY | O_APPEND, 0, fd1));
 		ft_printf("%s exists and is not a regular file\n", right.c);
 	}
 	else if (!access(right.c, F_OK))
 		ft_printf("%s exists but is not a writeable file\n", right.c);
 	else
-		return (dup2(open(right.c, O_WRONLY | O_APPEND | O_CREAT), fd1));
+		return (open_on_fd(right.c, O_WRONLY | O_APPEND | O_CREAT, CREATE_RIGHTS, fd1));
 	return (-1);
 }
 
@@ -83,23 +98,21 @@ int	i_greatand(t_elem left, t_elem right)
 	int fd1;
 
 	fd1 = left.c ? *left.c - '0' : 1;
-	if (dup2(*right.c - '0', fd1) == -1)
-		ft_printf("error : %s : incorrect fd\n", right.c);
-	return (0);
+	if (dup2(*right.c - '0', fd1) != -1)
+		return (fd1);
+	ft_printf("error : %s : incorrect fd\n", right.c);
+	return (-1);
 }
 
 int	i_lessgreat(t_elem left, t_elem right)
 {
-	(void) left;
-	(void) right;
-	int fd2;
+	int fd1;
 
-	fd2 = open(right.c, O_RDWR | O_CREAT);
-	if (fd2 == -1)
-		ft_printf("error : could not open %s\n", right.c);
-	else if (left.c)
-		dup2(fd2, *left.c - '0');
-	return (fd2);
+	fd1 = left.c ? *left.c - '0' : 0;
+	if (open_on_fd(right.c, O_RDWR | O_CREAT, CREATE_RIGHTS, fd1) != -1)
+		return (fd1);
+	ft_printf("error : could not open %s\n", right.c);
+	return (-1);
 }
 
 int	i_clobber(t_elem left, t_elem right)
