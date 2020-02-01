3eb016b3038ef1b9603e292a33b742211c531e98
G89Cl215 <thomas.gwed@gmail.com>
2020-02-01 14:19:50 +0100

    Fix(echo): final '\n'
    Now diplayed when no args are given.

--- src/builtins/builtin_echo.c
+++ src/builtins/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 20:52:32 by abarthel          #+#    #+#             */
-/*   Updated: 2020/01/19 13:39:20 by tgouedar         ###   ########.fr       */
+/*   Updated: 2020/02/01 14:18:58 by tgouedar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 int		cmd_echo(int argc, char **argv)
 {
-	int	i;
+	int		i;
 
 	i = 1;
 	while (i < argc)
 	{
 		ft_putstr(argv[i++]);
-		ft_putstr(i == argc ? "\n" : " ");
+		if (i < argc)
+			ft_putstr(" ");
 	}
+	ft_putstr("\n");
 	return (0);
 }
