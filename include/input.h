3a86708787c29e2980929e1349bd2304ebc2ef10
G89Cl215 <thomas.gwed@gmail.com>
2020-02-09 18:26:17 +0100

    Feat(CTRL CODES):
    Completion of all CTRL+alphabet codes.

--- include/input.h
+++ include/input.h
@@ -6,7 +6,7 @@
 /*   By: bprunevi <bprunevi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 12:11:17 by bprunevi          #+#    #+#             */
-/*   Updated: 2020/01/15 12:39:07 by bprunevi         ###   ########.fr       */
+/*   Updated: 2020/02/09 14:09:12 by tgouedar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,32 @@
 # define INPUT_H
 
 # define COUNT_KEY	 8
-# define CTRL_L		12
-# define CTRL_R		18
+# define CTRL_A		0x01
+# define CTRL_B		0x02
+# define CTRL_C		0x03
+# define CTRL_D		0x04
+# define CTRL_E		0x05
+# define CTRL_F		0x06
+# define CTRL_G		0x07
+# define CTRL_H		0x08
+# define CTRL_I		0x09
+# define CTRL_J		0x0a
+# define CTRL_K		0x0b
+# define CTRL_L		0x0c
+# define CTRL_M		0x0d
+# define CTRL_N		0x0e
+# define CTRL_O		0x0f
+# define CTRL_P		0x10
+# define CTRL_Q		0x11
+# define CTRL_R		0x12
+# define CTRL_S		0x13
+# define CTRL_T		0x14
+# define CTRL_U		0x15
+# define CTRL_V		0x16
+# define CTRL_W		0x17
+# define CTRL_X		0x18
+# define CTRL_Y		0x19
+# define CTRL_Z		0x1a
 # define ESC		'\033'
 # define INPUT_SIZE	256
 
@@ -67,6 +91,5 @@ int					set_reader(union u_tc *term, char **buff, t_cursor *cursor);
 int					keyboard_enter(union u_tc *term,char **buff, t_cursor *cursor);
 void				update_buff(char **buff, t_cursor *cursor);
 int					ft_init_tab(void);
-
 
 #endif
