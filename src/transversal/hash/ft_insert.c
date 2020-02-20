171da76b37f40e8e9585f97a38d09999fd4cfb40
G89Cl215 <thomas.gwed@gmail.com>
2020-02-20 15:09:28 +0100

    Fix(hash): Duplicate entries
    Inserting an entry with a key that is allready used will now wipe the
    previous entry in the table.

--- src/transversal/hash/ft_insert.c
+++ src/transversal/hash/ft_insert.c
@@ -6,7 +6,7 @@
 /*   By: tgouedar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 12:52:54 by tgouedar          #+#    #+#             */
-/*   Updated: 2020/02/12 15:31:49 by tgouedar         ###   ########.fr       */
+/*   Updated: 2020/02/20 15:08:13 by tgouedar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,40 @@ static void			ft_resize_htable(t_htable *htable)
 	ft_lstdel((t_list**)&entries, ft_get_free(htable->data_type));
 }
 
+static int			ft_redefine_entry(t_hlist **list, char *key, int type)
+{
+	t_hlist		*voyager;
+	t_hlist		*prev;
+
+	if (!(voyager = *list))
+		return (0);
+	prev = *list;
+	if (!(ft_strcmp(key, voyager->content->key)))
+	{
+		*list = voyager->next;
+		ft_lstdelone((t_list**)&voyager, ft_get_free(type));
+		return (1);
+	}
+	while (voyager->next)
+	{
+		voyager = voyager->next;
+		if (!(ft_strcmp(key, voyager->content->key)))
+		{
+			prev->next = voyager->next;
+			ft_lstdelone((t_list**)&voyager, ft_get_free(type));
+			return (1);
+		}
+		prev = voyager;
+	}
+	return (0);
+}
+
 void				ft_insert(t_htable *htable, char *key, void *value)
 {
 	int				data_type;
-	size_t			index;
 	t_entry			content;
 	t_hlist			*new_entry;
+	t_hlist			**lst;
 	t_ft_lstcpy		ft_cpy;
 
 	data_type = htable->data_type;
@@ -52,9 +80,10 @@ void				ft_insert(t_htable *htable, char *key, void *value)
 	new_entry = (t_hlist*)ft_lstnew(&content, sizeof(content));
 	ft_cpy = ft_get_lstcpy(data_type);
 	ft_cpy((t_list*)new_entry);
-	index = ft_hash(htable, new_entry->content->key);
-	ft_lstadd((t_list**)&(htable->table[index]), (t_list*)new_entry);
-	(htable->entry_nbr)++;
+	lst = &(htable->table[ft_hash(htable, new_entry->content->key)]);
+	if (!(ft_redefine_entry(lst, key, htable->data_type)))
+		(htable->entry_nbr)++;
+	ft_lstadd((t_list**)lst, (t_list*)new_entry);
 	if (htable->entry_nbr * 100 / htable->table_size > 70)
 		ft_resize_htable(htable);
 }
