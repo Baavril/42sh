bf320023e64a98d1f7d0cdda3c6684b78862d3c9
G89Cl215 <thomas.gwed@gmail.com>
2020-02-29 19:02:08 +0100

    Fix(Norme): 26 lines in a function
    ft_plop isn't a good name...

---
+++ src/transversal/pattern_matching/ft_alpharange.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_alpharange.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgouedar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/29 18:58:56 by tgouedar          #+#    #+#             */
+/*   Updated: 2020/02/29 18:59:27 by tgouedar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "globing.h"
+#include "expansions.h"
+#include "shell_variables.h"
+#include "builtins.h"
+#include "libft.h"
+
+char		*ft_alpharange(char c, char x)
+{
+	int		i;
+	int		lim;
+	char	*ret;
+
+	i = 0;
+	ret = NULL;
+	lim = (x - c) + 1;
+	if (lim > 1)
+	{
+		if (!(ret = (char *)malloc(sizeof(char) * (lim + 1))))
+			return (0);
+		while (i < lim)
+			ret[i++] = c++;
+		ret[i] = '\0';
+	}
+	return (ret);
+}
