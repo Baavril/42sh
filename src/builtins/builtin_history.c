cde99e59da7ea40fb96c5dd9ff50537716d0b585
G89Cl215 <thomas.gwed@gmail.com>
2020-03-08 10:52:40 +0100

    Chore(Norme): bultin_history.c

--- src/builtins/builtin_history.c
+++ src/builtins/builtin_history.c
@@ -6,12 +6,14 @@
 /*   By: yberramd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 11:34:30 by yberramd          #+#    #+#             */
-/*   Updated: 2020/02/29 15:25:45 by yberramd         ###   ########.fr       */
+/*   Updated: 2020/03/08 10:51:45 by tgouedar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "history.h"
 #include "libft.h"
+
+char			*g_progname;
 
 static int		ft_atoi_history(const char *str)
 {
@@ -34,7 +36,7 @@ static int		ft_atoi_history(const char *str)
 	return (nbr);
 }
 
-static int	history_nbr(int argc, char **argv)
+static char		*twoarg_history(char **argv)
 {
 	int		i;
 	int		max;
@@ -43,45 +45,52 @@ static int	history_nbr(int argc, char **argv)
 
 	i = 1;
 	max = 1;
+	nbr = ft_atoi_history(argv[1]);
 	history(FIRST, NULL, &cmd);
-	if (argc == 2)
+	while (history(FORWARD, NULL, &cmd) != 2 && cmd)
+		max++;
+	max -= nbr;
+	history(FIRST, NULL, &cmd);
+	if (history(GET, NULL, &cmd) && (i - max) > 0)
+		ft_printf("\t%d\t%s\n", i, cmd);
+	while (history(FORWARD, NULL, &cmd) != 2 && cmd)
 	{
-		nbr = ft_atoi_history(argv[1]);
-		while (history(FORWARD, NULL, &cmd) != 2 && cmd)
-			max++;
-		max -= nbr;
-		history(FIRST, NULL, &cmd);
-		if (history(GET, NULL, &cmd) && (i - max) > 0)
+		i++;
+		if ((i - max) > 0)
 			ft_printf("\t%d\t%s\n", i, cmd);
-		while (history(FORWARD, NULL, &cmd) != 2 && cmd)
-		{
-			i++;
-			if ((i - max) > 0)
-				ft_printf("\t%d\t%s\n", i, cmd);
-		}
 	}
+	return (cmd);
+}
+
+static int		history_nbr(int argc, char **argv)
+{
+	int		i;
+	char	*cmd;
+
+	i = 1;
+	if (argc == 2)
+		cmd = twoarg_history(argv);
 	else
 	{
+		history(FIRST, NULL, &cmd);
 		if (history(GET, NULL, &cmd))
 			ft_printf("\t%d\t%s\n", i, cmd);
 		while (history(FORWARD, NULL, &cmd) != 2 && cmd)
-		{
-			i++;
-			ft_printf("\t%d\t%s\n", i, cmd);
-		}
+			ft_printf("\t%d\t%s\n", ++i, cmd);
 	}
-	if (cmd)
-		return (1);
-	return (0);
+	return ((cmd) ? 1 : 0);
 }
 
-int		cmd_history(int argc, char **argv)
+int				cmd_history(int argc, char **argv)
 {
 	if (argc == 1 || (ft_isdigit(argv[1][0]) && argc == 2))
 		return (history_nbr(argc, argv));
 	else if (argc >= 2 && ft_isdigit(argv[1][0]))
-		ft_dprintf(2, "42sh: history: too many arguments\n");
+		ft_dprintf(STDERR_FILENO, \
+				"%s: history: too many arguments\n", g_progname);
 	else if (argc >= 2 && argv[1][0] != '-')
-		ft_dprintf(2, "42sh: history: %s: numeric argument required\n", argv[1]);
+		ft_dprintf(STDERR_FILENO,\
+				"%s: history: %s: numeric argument required\n", \
+				g_progname, argv[1]);
 	return (0);
 }
--- src/transversal/program/error.c
+++ src/transversal/program/error.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 16:56:23 by abarthel          #+#    #+#             */
-/*   Updated: 2020/01/15 15:29:26 by tgouedar         ###   ########.fr       */
+/*   Updated: 2020/03/08 10:51:51 by tgouedar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,31 +19,31 @@ char	*g_progname;
 
 const struct s_error_desc	g_errordesc[] =
 {
-	{ 0, "no error" },
-	{ 1, "invalid input" },
-	{ 1, "syntax error near unexpected token" },
-	{ 1, "no such builtin" },
-	{ 1, "bad substitution" },
-	{ 1, "cannot assign list to array member" },
-	{ 1, "not found" },
+	{ 0, "no error." },
+	{ 1, "invalid input." },
+	{ 1, "syntax error near unexpected token." },
+	{ 1, "no such builtin." },
+	{ 1, "bad substitution." },
+	{ 1, "cannot assign list to array member." },
+	{ 1, "not found." },
 	{ 2, "filename argument required\n.: usage: . filename [arguments]" },
-	{ 2, "cannot allocate memory" },
-	{ 2, "system call error" },
-	{ 126, "permission denied" },
-	{ 126, "is a directory" },
-	{ 127, "command not found" },
-	{ 127, "no such file or directory" },
-	{ 128, "resource temporarily unavailable" },
-	{ 1, "division by zero" },
-	{ 1, "attempted assignment to non-variable" },
-	{ 1, "operand expected" },
-	{ 1, "operator expected" },
-	{ 1, "two argumets to unary expression" },
-	{ 1, "value too great for base" },
-	{ 1, "invalid variable name" },
-	{ 1, "invalid number" },
-	{ 1, "invalid arithmetic base" },
-	{ 1, "exponent less than zero" }
+	{ 2, "cannot allocate memory." },
+	{ 2, "system call error." },
+	{ 126, "permission denied." },
+	{ 126, "is a directory." },
+	{ 127, "command not found." },
+	{ 127, "no such file or directory." },
+	{ 128, "resource temporarily unavailable." },
+	{ 1, "division by zero." },
+	{ 1, "attempted assignment to non-variable." },
+	{ 1, "operand expected." },
+	{ 1, "operator expected." },
+	{ 1, "two argumets to unary expression." },
+	{ 1, "value too great for base." },
+	{ 1, "invalid variable name." },
+	{ 1, "invalid number." },
+	{ 1, "invalid arithmetic base." },
+	{ 1, "exponent less than zero." }
 };
 
 void	psherror(int e_error, char *str, int e_message_type)
