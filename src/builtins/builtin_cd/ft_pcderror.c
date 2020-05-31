99c1fc27229e864dbc3ce017c7f25309f4f41e6e
G89Cl215 <thomas.gwed@gmail.com>
2020-05-31 16:52:18 +0200

    Fix(error): message naming
    cd errors were incorrect due to expansion in enum and psherror not called with error name.

--- src/builtins/builtin_cd/ft_pcderror.c
+++ src/builtins/builtin_cd/ft_pcderror.c
@@ -6,7 +6,7 @@
 /*   By: tgouedar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 17:26:37 by tgouedar          #+#    #+#             */
-/*   Updated: 2020/05/27 16:09:32 by tgouedar         ###   ########.fr       */
+/*   Updated: 2020/05/31 16:51:47 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,16 @@ int				ft_pcderror(int error_type)
 	if (error_type == TOO_MANY_ARG)
 		ft_dprintf(STDERR_FILENO, "%s: cd: Too many arguments.\n", g_progname);
 	if (error_type == UNSET_HOME)
-		psherror(27, "cd", e_builtin_type);
+		psherror(e_home_not_set, "cd", e_builtin_type);
 	else if (error_type == UNSET_OLDPWD)
-		psherror(28, "cd", e_builtin_type);
+		psherror(e_oldpwd_not_set, "cd", e_builtin_type);
 	else if (error_type == NO_PERM)
-		psherror(29, "cd", e_builtin_type);
+		psherror(e_permission_denied, "cd", e_builtin_type);
 	else if (error_type == NOT_DIR)
-		psherror(30, "cd", e_builtin_type);
+		psherror(e_not_a_directory, "cd", e_builtin_type);
 	else if (error_type == TARGET_NOT_FOUND)
-		psherror(31, "cd", e_builtin_type);
+		psherror(e_No_such_file_or_directory, "cd", e_builtin_type);
 	else if (error_type == EXCEED_LINK_LVL)
-		psherror(32, "cd", e_builtin_type);
+		psherror(e_too_many_levels_of_symbolic_links, "cd", e_builtin_type);
 	return (1);
 }
