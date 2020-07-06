daf4a6f5958426f9c1f01e5b23876c5e74831434
G89Cl215 <thomas.gwed@gmail.com>
2020-07-06 16:03:01 +0200

    Fix(quotes): quoted tokens
    No longer cuts last character of in-between quoted tokens.
    Now ignores backslashed quotes.

--- src/expansions/global_exp/quotes.c
+++ src/expansions/global_exp/quotes.c
@@ -6,7 +6,7 @@
 /*   By: baavril <baavril@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 20:52:32 by baavril           #+#    #+#             */
-/*   Updated: 2020/01/15 16:39:30 by baavril          ###   ########.fr       */
+/*   Updated: 2020/07/06 15:43:32 by tgouedar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ struct s_quoted	*g_quoted;
 
 static int		reachquotelim(char *str, char c)
 {
-	int	i;
+	int		i;
 
 	i = 1;
 	while (str[i] && str[i] != c)
@@ -28,13 +28,12 @@ static int		reachquotelim(char *str, char c)
 		if (str[i] == c && str[i - 1] == BSLASH)
 			++i;
 	}
-	++i;
 	return (i);
 }
 
 int				getquotelim(char *str)
 {
-	int	i;
+	int		i;
 
 	i = 0;
 	if (str[i] == DQUOTES)
@@ -43,19 +42,20 @@ int				getquotelim(char *str)
 		return (reachquotelim(&str[i], SQUOTES));
 	else if (str[i] != SQUOTES && str[i] != DQUOTES)
 	{
-		++i;
-		while (str[i] && str[i] != DQUOTES && str[i] != SQUOTES)
+		while ((str[i + 1] && str[i + 1] != DQUOTES && str[i + 1] != SQUOTES)
+		|| str[i] == '\\')
 			++i;
-		return (i);
+		if (!(str[i + 1]))
+			++i;
 	}
 	return (i);
 }
 
 int				counter_quoted_words(char *str)
 {
-	int	i;
-	int	k;
-	int	len;
+	int		i;
+	int		k;
+	int		len;
 
 	i = 0;
 	k = 0;
@@ -63,6 +63,8 @@ int				counter_quoted_words(char *str)
 	while (i < len && str[i])
 	{
 		i += getquotelim(&str[i]);
+		if (str[i] != DQUOTES || str[i] != SQUOTES)
+			++i;
 		++k;
 	}
 	return (k);
@@ -73,7 +75,7 @@ void			setquotenod(struct s_quoted *new_back)
 	struct s_quoted	*voyager;
 
 	if (!g_quoted)
-		return ;
+		g_quoted = new_back;
 	else
 	{
 		voyager = g_quoted;
