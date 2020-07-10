86c929f23dcf7d59bdbb086230ad3d6aa5091ede
G89Cl215 <thomas.gwed@gmail.com>
2020-07-10 14:23:53 +0200

    Fix(path_type): stat error
    Now handling stat errors.

--- src/interpreter/path_type.c
+++ src/interpreter/path_type.c
@@ -6,7 +6,7 @@
 /*   By: tgouedar <tgouedar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 01:19:32 by tgouedar          #+#    #+#             */
-/*   Updated: 2020/07/09 01:21:16 by tgouedar         ###   ########.fr       */
+/*   Updated: 2020/07/10 14:16:45 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int				is_regfile(const char *path)
 {
 	struct stat	path_stat;
 
-	stat(path, &path_stat);
+	if (stat(path, &path_stat))
+		return (0);
 	return (S_ISREG(path_stat.st_mode) || S_ISCHR(path_stat.st_mode));
 }
 
@@ -24,6 +25,7 @@ int				is_dir(const char *path)
 {
 	struct stat	path_stat;
 
-	stat(path, &path_stat);
+	if (stat(path, &path_stat))
+		return (0);
 	return (S_ISDIR(path_stat.st_mode));
 }
